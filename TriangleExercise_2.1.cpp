bcf9495b25ccb63105a8ede494475b6dae114ca4
Tina Zejda <murp443@gmail.com>
2024-04-29 22:48:39 -0700

    tri ex 2.1

--- TriangleExercise_2.1.cpp
+++ TriangleExercise_2.1.cpp
@@ -1,1 +1,121 @@
-#include 
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <math.h>
+using namespace std;
+
+// setting up fragment and vertex shaders (source code)
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(0.0f, 0.0f, 0.00f, 1.0f);\n"
+"}\n\0";
+
+int main() {
+	
+	glfwInit();
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "square time", NULL, NULL);
+	if(window == NULL) {
+		cout << "no window..." << endl;
+		glfwTerminate();
+		return -1;
+	}	  
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+
+	GLfloat vertices[] = {
+		// upper right triangle
+		0.0f, 0.0f, 0.0f, // lower left
+		0.5f, 0.0f, 0.0f, // lower right
+		0.5f, 0.5f, 0.0f, // upper right
+		// lower left triangle
+		0.0f, 0.0f, 0.0f, // lower left
+		0.0f, 0.5f, 0.0f, // upper left
+		0.5f, 0.5f, 0.0f, // upper right 
+	
+		// 2 triangles make a square! :)
+		// GL_QUAD and GL_POLYGON don't work for 4 points for some reason, but im probably just doing somethin wrong
+	};
+
+	GLuint VAO, VBO;
+	
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// change 1 of 3s to 4 bc 4 vertices?
+	// 1st 3 = how many values per vertex
+	// 2nd 3 = amt of data b/w vertices?
+	// should be fine...
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	while(!glfwWindowShouldClose(window)) {
+		// background
+		glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+
+		glBindVertexArray(VAO);
+
+		// primitive=square, starting index=0, num vertices to draw=4
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
