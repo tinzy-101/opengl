f961afec497feedceb31146fe93eb9860fe2e157
Tina Zejda <murp443@gmail.com>
2024-05-31 06:41:42 -0700

    finish index buffer tutorial

--- indexBuffer.cpp
+++ indexBuffer.cpp
@@ -84,36 +84,50 @@ int main() {
 		0, 3, 5, // lower left triangle
 		3, 2, 4, // lower right tri
 		5, 4, 1 // upper tri
-	}
+	};
 
 	// create reference value for index buffer (very similar to code for VBO)
 	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	// specify only have 1 object for index buffer 
 	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// make the index buffer current (bind it) so it can be used
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// link EBO to indices array
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	// unbind the index buffer, make sure to do AFTER unbind VAO, bc EBO is stored in VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 
 	while(!glfwWindowShouldClose(window)) {
 		glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// primitive we want to draw, num indices to draw, data type of indices, starting index 
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		//REPLACED: glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 	  	glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
